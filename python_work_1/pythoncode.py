59ba009016586ce2971e3314d9ada07819fd29a5
liyaya <1564088177@qq.com>
2021-06-23 11:51:56 +0800

    "python作业1"

---
+++ python_work_1/pythoncode.py
@@ -0,0 +1,15 @@
+# print(open("README.md", "w"))
+# global是全局的
+class Person:
+    def setu(self, name):
+        print(f"有参数{name == 2}")
+
+    def seuu(self):
+        print("无参数")
+
+    def se(self):
+        print("有返回值")
+        return 1
+
+    def s(self):
+        print("无返回值")
